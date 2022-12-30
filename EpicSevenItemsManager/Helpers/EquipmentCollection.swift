a9387a53eb183f186b8dd9a6ca76327327b58f6b
blue-sis <hudsonsun2078@gmail.com>
2022-12-29 21:23:46 -0500

    added stats icons to assets

---
+++ EpicSevenItemsManager/Helpers/EquipmentCollection.swift
@@ -0,0 +1,13 @@
+//
+//  EquipmentCollection.swift
+//  EpicSevenItemsManager
+//
+//  Created by Haobo Sun on 2022-12-29.
+//
+
+import Foundation
+
+public class EquipmentCollection {
+    public static var bombModelKanna = [Weapon(), ]
+    
+}
